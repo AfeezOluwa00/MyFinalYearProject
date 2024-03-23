5ce04eb48f59794e24c2cc57df7df16fc17e85d3
Afeez <Afeezoluwa@yahoo.ie>
2024-03-23 13:10:35 +0000

    g

---
+++ app/src/main/java/com/sickle/healthcareapp/MoodTrackerActivity.java
@@ -0,0 +1,16 @@
+package com.sickle.healthcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mysicklecellapp.R;
+
+public class MoodTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_tracker);
+    }
+}
---
+++ app/src/main/java/com/sickle/healthcareapp/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.sickle.healthcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mysicklecellapp.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
